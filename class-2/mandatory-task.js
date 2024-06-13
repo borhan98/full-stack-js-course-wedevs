5f3a7148c606494666d1a7a1bf4f562dc9cd57e6
borhan98 <borhanuddindns420@gmail.com>
2024-06-13 19:50:03 +0600

    Done upto optional task 3

---
+++ class-2/mandatory-task.js
@@ -0,0 +1,108 @@
+/*---------------------------------------------------------
+                        Task-1 
+-----------------------------------------------------------*/
+// var a = 5;
+// var b = 15;
+// if (a > b) {
+//     console.log('a is greater than b');
+// } else {
+//     console.log('a is not greater than b');
+// }
+
+// function multiply(x, y) {
+//     return x * y;
+// }
+// console.log(multiply(3, 7)); //multiply 3 and 7
+
+// var c = 8;
+// var d = 8;
+// if (c === d) {
+//     console.log('c is equal to d');
+// } else {
+//     console.log('c is not equal to d');
+// }
+
+// function subtract(m, n) {
+//     return m - n;
+// }
+// console.log(subtract(10, 4)); //subtract 10 and 4
+
+// var e = 12;
+// var f = 6;
+// if (e % f === 0) {
+//     console.log('e is divisible by f');
+// } else {
+//     console.log('e is not divisible by f');
+// }
+
+// function divide(p, q) {
+//     return p / q;
+// }
+// console.log(divide(20, 5)); //divide 20 by 5
+
+/*----------------------------------------------------------
+                    Task-:2
+------------------------------------------------------------*/
+// // Arithmetic Operations
+// let a = 10;
+// let b = 5;
+// console.log(a + b); // 15
+// console.log(a - b); // 5
+// console.log(a * b); // 50
+// console.log(a / b); // 2
+// console.log(a % b); // 0
+
+// // Comparison Operations
+// console.log(a > b); // true
+// console.log(a < b); // false
+// console.log(a == 10); // true
+// console.log(b != 5); // false
+
+// // Logical Operations
+// let isAdult = true;
+// let hasPermission = false;
+// console.log(isAdult && hasPermission); // false
+// console.log(isAdult || hasPermission); // true
+// console.log(!isAdult); // false
+
+// // Ternary Operation
+// let age = 20;
+// let access = (age >= 18) ? 'Granted' : 'Denied';
+// console.log(access); // Outputs: Granted
+
+
+/*-------------------------------------------------------------
+                        Task-3
+---------------------------------------------------------------*/
+
+// addition function
+function add(x, y) {
+    return x + y;
+}
+const addition1 = add(4, 5);
+const addition2 = add(7, 9);
+console.log(addition1, addition2); // output: 9, 16
+
+// substraction function
+function subtract(x, y) {
+    return x - y;
+}
+const substract1 = subtract(5, 2);
+const substract2 = subtract(5, 8);
+console.log(substract1, substract2); // output: 3, -3
+
+// multiplication function
+function multiply(x, y) {
+    return x * y;
+}
+const multiply1 = multiply(4, 5);
+const multiply2 = multiply(3, 7);
+console.log(multiply1, multiply2); // output: 20, 21
+
+// division function
+function divide(x, y) {
+    return x / y;
+}
+const division1 = divide(50, 5);
+const division2 = divide(20, 5);
+console.log(division1, division2); // output: 10, 4
---
+++ class-2/optional-task.js
@@ -0,0 +1,89 @@
+/*-------------------------------------------------------------
+                        Task-1
+---------------------------------------------------------------*/
+
+// for loop
+function printNumbers() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+printNumbers(); // output: 1 2 3 4 5 6 7 8 9 10
+
+// while loop
+function printEvenNumbers() {
+    let i = 2;
+
+    while (i <= 20) {
+        console.log(i);
+        i += 2;
+    }
+}
+printEvenNumbers(); // output: 2 4 6 8 10 12 14 16 18 20
+
+// do while loop
+function printOddNumbers() {
+    let i = 1;
+    do {
+        console.log(i);
+        i += 2;
+    } while (i <= 19);
+}
+printOddNumbers();
+
+/*-------------------------------------------------------------
+                        Task-2
+---------------------------------------------------------------*/
+// day of the week
+function getDayName(dayNumber) {
+    let day;
+    switch (dayNumber) {
+        case 1:
+            day = "Sunday";
+            break;
+        case 2:
+            day = "Monday";
+            break;
+        case 3:
+            day = "Tuesday";
+            break;
+        case 4:
+            day = "Webnesday";
+            break;
+        case 5:
+            day = "Thursday";
+            break;
+        case 6:
+            day = "Friday";
+            break;
+        case 7:
+            day = "Saturday";
+            break;
+        default:
+            day = "Please provide a valid day number...!!!";
+            break;
+    }
+    return day;
+}
+
+console.log(getDayName(0)); // Please provide a valid day number...!!!
+console.log(getDayName(6)); // Friday
+console.log(getDayName(7)); // Saturday
+
+
+/*-------------------------------------------------------------
+                        Task-3
+---------------------------------------------------------------*/
+function testFunctionScope() {
+    let insideFunction = function () { }; // It's an inside function
+}
+// console.log(insideFunction()); // ReferenceError: insideFunction is not defined
+
+
+function testBlockScope() {
+    if (true) {
+        let insideVariable = "This is an variable inside a block";
+    }
+    console.log(insideVariable);
+}
+testBlockScope(); // RefferenceError: insideVariable is not defined
